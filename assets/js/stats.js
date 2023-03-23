cf4a39db1fc2b11b93e63d46827629a8d3e8d591
barreirojm <barreirojuanmanuel@gmail.com>
2023-03-22 21:11:57 -0300

    inicio stats con vue

---
+++ assets/js/stats.js
@@ -0,0 +1,23 @@
+const { createApp } = Vue
+
+const url = 'https://mh.up.railway.app/api/amazing-events'
+const urlUpcoming = 'https://mh.up.railway.app/api/amazing-events?time=upcoming'
+const urlPast = 'https://mh.up.railway.app/api/amazing-events?time=past'
+
+const app = createApp({
+    data(){         /* funcion que define los estados (propiedades reactivas) de la aplicación */
+        return {
+            /* clave: valor */
+        }
+    },
+    created() {     /* hook con las funciones que se ejecutan luego de cargar las variables de estado */
+        /* ejecuto funcion() */
+    },
+    methods: {      /* objeto con todos los métodos que necesita la app (fetch/filter/etc) */
+        /* defino funcion */
+    },
+    computed: {     /* objeto con todos los métodos que se ejectuan automaticamente cuando CAMBIA un estado */
+    }
+})
+
+app.mount("#app")   /* monto la app de VUE */
