3f58d37c2c63e152ef6531e8bbc6be29e30e5a94
barreirojm <barreirojuanmanuel@gmail.com>
2023-03-22 21:08:16 -0300

    formulario de contacto finalizado 2

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -22,7 +22,10 @@ const { createApp } = Vue
                 email: this.email,
                 mensaje: this.mensaje
             }
-            console.log(datosFormulario);
+            
+            if (!Object.values(datosFormulario).some(valor => valor === '')) {
+            console.log(datosFormulario)
+            };
            
             if (!Object.values(datosFormulario).some(valor => valor === '')) {
                 alert(`Hi there `+this.nombre+' thanks for reaching out.\nWe have received your message and will get back to you as soon as possible at your email: '+this.email+'\nAmazing Events.-');
