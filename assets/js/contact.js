571cbf6b4672bd3bc658f9741e99eef9674b76d9
barreirojm <barreirojuanmanuel@gmail.com>
2023-03-22 21:06:31 -0300

    formulario de contacto finalizado

---
+++ assets/js/contact.js
@@ -0,0 +1,38 @@
+const { createApp } = Vue
+
+
+  createApp({
+    data() {
+      return {   
+        nombre: "",
+        email: "",
+        mensaje: ""       
+        
+      }
+    },
+    created(){
+        this.captureData()
+        
+    },
+    
+    methods: {
+        captureData() {
+            const datosFormulario = {
+                nombre: this.nombre,
+                email: this.email,
+                mensaje: this.mensaje
+            }
+            console.log(datosFormulario);
+           
+            if (!Object.values(datosFormulario).some(valor => valor === '')) {
+                alert(`Hi there `+this.nombre+' thanks for reaching out.\nWe have received your message and will get back to you as soon as possible at your email: '+this.email+'\nAmazing Events.-');
+            } 
+        }
+        
+    },
+   
+    computed: {
+
+    }
+    
+}).mount('#app')
